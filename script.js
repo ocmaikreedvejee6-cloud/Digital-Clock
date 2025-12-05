bdbfc58eab73a2c7c28ca81fa21d30fc73bd9ac8
ocmaikreedvejee6-cloud <ocmaikreedvejee6@gmail.com>
2025-12-05 14:08:22 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function updateClock() {
+    let now = new Date();
+    let hours = String(now.getHours()).padStart(2, "0");
+    let minutes = String(now.getMinutes()).padStart(2, "0");
+    let seconds = String(now.getSeconds()).padStart(2, "0");
+
+    document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); // initial call
